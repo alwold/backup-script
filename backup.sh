a7c6801cf20a697668573edc1a7fd76cbdad012c
Al Wold <alwold@gmail.com>
2012-04-30 12:42:44 -0700

    update the dumpdates file

--- backup.sh
+++ backup.sh
@@ -14,7 +14,7 @@ errors=0
 # TODO check if dump volume is mounted?
 
 # backup the root partition
-/sbin/dump ${dump_level}f ${DUMP_DIR}/dump-${dump_level}-${date_stamp}.dump / &> $log_file
+/sbin/dump ${dump_level}uf ${DUMP_DIR}/dump-${dump_level}-${date_stamp}.dump / &> $log_file
 if [ "$?" -ne "0" ]
   then errors=1
 fi
